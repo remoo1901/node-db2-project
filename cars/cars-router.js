5c9c13c18c866a4482a608ef3a3abac37ec5ebb0
remoo1901 <remoo1901@hotmail.com>
2020-06-19 22:28:29 -0400

    working on migration

---
+++ cars/cars-router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const db = require("../data/config");
+
+const router = xpress - Router();
+
+//==================
+// GET Cars
+//==================
+
+router.get("/", async (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const server = require("./server.js")
+const express = require("express")
+
+const server = express()
 
 const PORT = process.env.PORT || 5000;
+
+server.use(express.json())
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+		message: "Something went wrong",
+	})
+
+})
+
+
 
 server.listen(PORT, () => {
     console.log(`\n== API running on port ${PORT} ==\n`)
